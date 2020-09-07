cc6069161ac7875799d3347702acd6a44620e862
NOVITZ Jean-Philippe <novitz@gmail.com>
2020-09-07 13:45:25 +0200

    .

--- wp-content/themes/portfoliobase/functions.php
+++ wp-content/themes/portfoliobase/functions.php
@@ -13,7 +13,7 @@ if (!function_exists('base_enqueue_styles')) {
         wp_enqueue_style( 'style', get_stylesheet_directory_uri().'/assets/css/app.css' );
 //        wp_enqueue_style( 'style', get_stylesheet_directory_uri().'/assets/css/style.css' );
 //        wp_enqueue_script('javascript', get_stylesheet_directory_uri().'/assets/js/main.js');
-        wp_enqueue_script('what-input', get_stylesheet_directory_uri().'/foundation-portfolio/node_modules/what-input/dist/what-input.js', array(), null);
+        wp_enqueue_script('what-input', get_stylesheet_directory_uri().'/foundation-portfolio/node_modules/what-input/dist/what-input.min.js', array(), null);
         wp_enqueue_script('foundation', get_stylesheet_directory_uri().'/foundation-portfolio/node_modules/foundation-sites/dist/js/foundation.js', array(), null);
 //        wp_enqueue_script('javascript', get_stylesheet_directory_uri().'/assets/js/app.js');
         wp_enqueue_script('app', get_stylesheet_directory_uri().'/assets/js/app.js');
