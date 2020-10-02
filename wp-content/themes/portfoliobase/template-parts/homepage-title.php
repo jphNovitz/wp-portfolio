7dae6ebd018f8b6ced9fda2759255b8f8ccf6d14
NOVITZ Jean-Philippe <novitz@gmail.com>
2020-10-02 15:07:40 +0200

    echo pitch

--- wp-content/themes/portfoliobase/template-parts/homepage-title.php
+++ wp-content/themes/portfoliobase/template-parts/homepage-title.php
@@ -8,58 +8,37 @@
  */
 ?>
 <!--<div class="row" id="frontrow" >-->
-
-<div class="title-bar small-margin-collapse" data-responsive-toggle="menu1" data-hide-for="medium">
-    <button class="menu-icon" type="button" data-toggle="menu1"></button>
-    <div class="title-bar-title"> Novitz Jean-Philippe </div>
-</div>
-
-<div class="top-bar" id="menu1">
-    <div class="top-bar-left ">
-        <ul class="dropdown vertical medium-horizontal  menu " data-dropdown-menu >
-            <li class="menu-text">Développeur Web</li>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Compétences</a>
-                <ul class="menu" >
-                    <li style="background-color: #fff"><a href="/#php">PHP</a></li>
-                    <li style="background-color: #fff"><a href="/#js">Javascript</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Expérience</a></li>
-            <li><a href="#">Réalisation</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+<nav>
+    <div class="title-bar small-margin-collapse" data-responsive-toggle="menu1" data-hide-for="medium">
+        <button class="menu-icon" type="button" data-toggle="menu1"></button>
+        <div class="title-bar-title"> Novitz Jean-Philippe</div>
     </div>
-<!--    <div class="top-bar-right">-->
-<!--        <ul class="menu">-->
-<!--            <li><a href="mailto:novitz@gmail.com"><i class="fas fa-at"></i></a></li>-->
-<!--            <li><a href="https://www.linkedin.com/in/jean-philippe-novitz-777566161/"><i-->
-<!--                        class="fab fa-linkedin"></i></a></li>-->
-<!--            <li><a href="https://twitter.com/jphNovitz"><i class="fab fa-twitter"></i></a></li>-->
-<!--            <li><a href="https://www.facebook.com/jphNovitz/"><i class="fab fa-facebook"></i></a></li>-->
-<!--            <li><a href="https://www.instagram.com/jphNovitz/"><i class="fab fa-instagram"></i></a></li>-->
-<!--        </ul>-->
-<!--    </div>-->
-</div>
-<div class="row align-middle" id="intro">
+
+    <div class="top-bar" id="menu1">
+        <div class="top-bar-left ">
+            <ul class="dropdown vertical medium-horizontal  menu " data-dropdown-menu>
+                <li class="menu-text">Développeur Web</li>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Compétences</a>
+                    <ul class="menu">
+                        <li style="background-color: #fff"><a href="/#php">PHP</a></li>
+                        <li style="background-color: #fff"><a href="/#js">Javascript</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Expérience</a></li>
+                <li><a href="#">Réalisation</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<header class="row align-middle" id="intro">
     <div class="column small-12 large-12 text-center">
         <h2>Bienvenue</h2>
     </div>
     <div id="" class="columns small-12 large-6  text-center">
-
-<!--        <p>--><?php //echo esc_attr__(get_option('pitch')); ?><!--</p>-->
         <p class="lead">
-            Développeur Php (<span class=""> Symfony</span>, Laravel, Prestashop) et Javascript (jQuery, Vue.js) disponible immédiatement.
-        </p>
-
-        <p class="lead">N'hesitez pas à me <a href="mailto:hello@jphnovitz.be" > contacter </a>pour ce que je sais faire (Site vitrines, Ecommerce, Api Rest, application Vue.js, socket.io) ou pour m'apporter
-            une nouvelle expérience dans ce que vous faite.
-<!--            Je suis un développeur WEB made in LIEGE, diplômé-->
-<!--            <br/> Formé à Symfony <span class="more-big">(2, 3, 4) </span> et Javascript <span class="more-big">(vanilla, jQuery) </span>-->
-<!--            j'ai ajouté <span class="more-big">des cordes à mon arc avec </span> VUE.JS LARAVEL, PRESTASHOP, <span-->
-<!--                class="more-big">aussi node et socket.io</span>.-->
-<!--            <span class="more-big"><br/>Si vous souhaiter utiliser ce que je sais ou si vous avez une autre corde pour moi.</span>-->
-<!--            Je suis disponible Immédiatement.-->
+            <?php echo(get_option('pitch')); ?>
         </p>
 
     </div>
@@ -93,4 +72,4 @@
             </div>
         </div>
     </div>
-</div>
+</header>
