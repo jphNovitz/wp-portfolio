db13f6307a3bde012ed33a1384159e3908cb716e
NOVITZ Jean-Philippe <novitz@gmail.com>
2020-11-10 20:13:42 +0100

    well advance

--- wp-content/themes/jphn_tullipa/template-parts/home/demos.php
+++ wp-content/themes/jphn_tullipa/template-parts/home/demos.php
@@ -9,8 +9,8 @@ $site = new  WP_Query([
 $x = 0;
 ?>
 
-<section id="demos" class="">
-    <div class="row ">
+<section id="demos" class="m2">
+    <div class="row m2 ">
         <div class="columns small-12 medium-4 large-5 align-middle">
             <h4 class="article-title">Ce que je fais</h4>
             <aside class=" big lead">
@@ -29,12 +29,12 @@ $x = 0;
         echo '<div class="expanded row">';
     endif;
     ?>
-    <article class="columns small-6 medium-6 large-3 card"
+    <article class="columns small-offset-1 small-5  medium-offset-1 medium-5 large-offset-1 large-3 card"
              style="background-image: url(
                      '<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>'">
         <div class="card-divider">
             <h4><?php the_title(); ?></h4>
-            <br/><a href="--><?php echo get_post_permalink(); ?>">
+            <br/><a href="<?php echo get_post_permalink(); ?>">
                 <i class="fi-arrow-right link size-72"></i>
             </a>
             <!--                            <a href="--><?php //echo get_post_permalink();
@@ -62,57 +62,3 @@ $x = 0;
 
 
 </section>
-
-***
-
-<section class="row m2" style="background-color: rgba(225, 226, 227, .95) ; margin-bottom: 10rem;">
-    <div class="columns small-12" id="demos">
-        <h3>Réalisations et démos</h3>
-    </div>
-    <?php
-    while ($site->have_posts()) :
-        $site->the_post();
-        if ($x == 0):
-            //    echo (get_the_ID());
-            ?>
-            <article class="columns small-12 medium-6 large-4  site-parent"
-                     style="background-image: url(
-                             '<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>'">
-                <div class="text-center site-child-1">
-                    <h3><a href="<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    <p>
-                        <?php the_excerpt(); ?>
-                    </p>
-                    <!--            <p>le code se trouve sur <a href="https://github.com/jphNovitz/sandwicherie">github</a></p>-->
-                </div>
-            </article>
-
-        <?php else:
-            if ($x == 1 || $x == 3):
-                if ($x == 1) echo "<div class='columns small-12 medium-6 large-8 '>";
-                echo "<div class='row site-parent-row' style='background-color: white ; height: 25vh ; '>";
-            endif; ?>
-
-
-            <article class="columns small-12 medium-6 large-6 site-parent-secondary "
-                     style=" background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>') ;">
-                <div class="site-child-1">
-                    <h4><a href="<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a></h4>
-                    <p><?php the_excerpt(); ?>  </p>
-                </div>
-            </article>
-            <?php if ($x == 2 || $x == 4):
-            echo "</div>";
-            if ($x == 5) echo "</div>";
-        endif; ?>
-        <?php
-
-        endif;
-        $x++;
-    endwhile;
-    ?>
-    </div>
-    <!--    </div>-->
-</section>
-
-
--- wp-content/themes/jphn_tullipa/template-parts/home/experience.php
+++ wp-content/themes/jphn_tullipa/template-parts/home/experience.php
@@ -1,101 +1,21 @@
-<section class="grid-x grid-padding-x" >
-    <article class="cell large-12" id="experience_pro">
-        <h2>Expériences professionnelles</h2>
-        <p>Junior est un point de départ, pour évoluer il faut des expériences professionnelles. Chacune des
-            entreprises
-            dans laquelle je suis passé m'a appris un tas de choses que ce soit au niveau d'une techno, d'un
-            language, d'un logiciel,
-            d'une organisation ou tout simplemenent au niveau humain.
-            <br/>Merci à elles.</p>
-    </article>
-
-    <div class="cell medium-6 large-3">
-
-        <article class="card ">
-            <div class="card-divider text-center">
-                <h3 class="text-center">OnlyWeb</h3>
-            </div>
-            <div class="card-section">
-                <ul>
-                    <li>9 mois en 2020</li>
-                    <li>Symfony 4</li>
-                    <li>jQuery</li>
-                    <li>Websockets</li>
-                </ul>
-                <p><a href="https://onlyweb.be">Onlyweb.be</a></p>
-            </div>
-        </article>
-
+<section id="experience" class="bg-secondary" style="padding: 2rem 0;">
+    <div class="row m2 ">
+        <div class="columns small-12 ">
+            <h4 class="article-title">Expériences professionnelles</h4>
+            <aside class=" big lead text-white">
+                Merci à ces entreprises
+            </aside>
+        </div>
     </div>
+    <div class="row m2 align-middle">
+        <div class="columns small-1 "><img src="<?php echo get_template_directory_uri();?>/assets/img/entreprises/logo_isl.png" /></div>
+        <div class="columns small-1 small-offset-1"><img src="<?php echo get_template_directory_uri();?>/assets/img/entreprises/logo_newave.png" /></div>
+        <div class="columns small-1 small-offset-1"><img src="<?php echo get_template_directory_uri();?>/assets/img/entreprises/logo_laclementine.png" /></div>
+        <div class="columns small-1 small-offset-1"><img src="<?php echo get_template_directory_uri();?>/assets/img/entreprises/logo_lsis.png" /></div>
 
-    <div class="cell medium-6 large-3">
-
-        <article class="card ">
-            <div class="card-divider">
-                <h3>Produweb</h3>
-            </div>
-            <div class="card-section">
-                <ul>
-                    <li>3 mois en 2019</li>
-                    <li>Prestashop</li>
-                </ul>
-                <p><a href="https://www.produweb.be">produweb.be</a></p>
-            </div>
-        </article>
-
-    </div>
-    <div class="cell medium-6 large-3">
-
-        <card class="card ">
-            <div class="card-divider">
-                <h3>La Clémentine</h3>
-            </div>
-            <div class="card-section">
-                <ul>
-                    <li>9 mois en 2018</li>
-                    <li>Symfony 5</li>
-                    <li>Vue.js</li>
-                    <li>Api Rest</li>
-                </ul>
-                <p><a href="http://laclementine.be">laclementine.be</a></p>
-            </div>
-        </card>
-
-    </div>
-    <div class="cell medium-6 large-3">
-
-        <article class="card">
-            <div class="card-divider">
-                <h3 class="text-center">LSIS</h3>
-            </div>
-            <div class="card-section">
-                <ul>
-                    <li>3 mois en 2018</li>
-                    <li>Laravel</li>
-                    <li>Wordpress</li>
-                    <li>jQuery</li>
-                </ul>
-                <p><a href="http://lsis.be">lsis.be</a></p>
-            </div>
-        </article>
-
-    </div>
-    <div class="cell medium-6 large-3">
-
-        <article class="card">
-            <div class="card-divider">
-                <h3>Newave</h3>
-            </div>
-            <div class="card-section">
-                <ul>
-                    <li>Stage 2 mois en 2015</li>
-                    <li>Wordpress</li>
-                    <li>jQuery</li>
-                    <li>Mobile First</li>
-                </ul>
-                <p><a href="http://www.newave.be">newave.be</a></p>
-            </div>
-        </article>
+        <div class="columns small-1 small-offset-1"><img src="<?php echo get_template_directory_uri();?>/assets/img/entreprises/logo_produweb.jpg" /></div>
+        <div class="columns small-1 small-offset-1"><img src="<?php echo get_template_directory_uri();?>/assets/img/entreprises/logo_bugiweb.webp" /></div>
+        <div class="columns small-1 "><img src="<?php echo get_template_directory_uri();?>/assets/img/entreprises/logo_bugiweb.png" /></div>
 
     </div>
 </section>
