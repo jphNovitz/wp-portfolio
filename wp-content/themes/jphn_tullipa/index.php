3d560b2a11a4e017dce1bfda640813074d7b607e
NOVITZ Jean-Philippe <novitz@gmail.com>
2020-11-11 22:00:34 +0100

    first try

---
+++ wp-content/themes/jphn_tullipa/index.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+?>
+
+<?php require_once (get_template_directory().'/template-parts/home/for-what.php'); ?>
+<?php require_once (get_template_directory().'/template-parts/home/demos.php'); ?>
+<?php //require_once (get_template_directory().'/template-parts/home/envir.php'); ?>
+<?php //require_once (get_template_directory().'/template-parts/home/js.php'); ?>
+
+<?php require_once (get_template_directory().'/template-parts/home/experience.php'); ?>
+<?php require_once (get_template_directory().'/template-parts/home/techno.php'); ?>
+
+
+<?php
+get_footer();
+?>
