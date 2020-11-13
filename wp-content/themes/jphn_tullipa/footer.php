b4f9c2b83cf9f5914ee360e778630dbe1d77d9ee
NOVITZ Jean-Philippe <novitz@gmail.com>
2020-11-13 20:09:49 +0100

    ..

--- wp-content/themes/jphn_tullipa/footer.php
+++ wp-content/themes/jphn_tullipa/footer.php
@@ -1,5 +1,6 @@
 <?php wp_footer(); ?>
-<!--<script src="foundation-portfolio/node_modules/jquery/dist/jquery.js"></script>-->
+<script src="/foundation-portfolio/node_modules/jquery/dist/jquery.js"></script>
+
 <!--<script src="foundation-portfolio/node_modules/what-input/dist/what-input.js"></script>-->
 <!--<script src="foundation-portfolio/node_modules/foundation-sites/dist/js/foundation.js"></script>-->
 <!--<script src="assets/js/app.js"></script>-->
--- wp-content/themes/jphn_tullipa/header.php
+++ wp-content/themes/jphn_tullipa/header.php
@@ -8,10 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">-->
 
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+   <!--  <script src="https://code.jquery.com/jquery-2.2.4.min.js"
             integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
             crossorigin="anonymous">
-    </script>
+    </script> -->
     <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js">
     </script>
 <!--    <script type='text/javascript' src='http://localhost:8080/wp-content/themes/portfoliobase/assets/js/app.js'></script>-->
