612b07eae4f8b8484a9c922bf1daa6f30ce63125
NOVITZ Jean-Philippe <novitz@gmail.com>
2020-11-13 20:17:47 +0100

    ..

--- wp-content/themes/jphn_tullipa/functions.php
+++ wp-content/themes/jphn_tullipa/functions.php
@@ -16,7 +16,7 @@ if (!function_exists('base_enqueue_styles')) {
 //        wp_enqueue_style( 'style', get_stylesheet_directory_uri().'/assets/css/style.css' );
         wp_enqueue_script('jquery', get_stylesheet_directory_uri(). '/foundation-portfolio/node_modules/jquery/dist/jquery.js', [], false, true);        
         wp_enqueue_script('javascript', get_stylesheet_directory_uri() . '/assets/js/main.js', [], true, true);
-        wp_enqueue_script('what-input', get_stylesheet_directory_uri() . '/foundation-portfolio/node_modules/what-input/dist/what-input.min.js', array(), null, true, true);
+        wp_enqueue_script('what-input', get_stylesheet_directory_uri() . '/foundation-portfolio/node_modules/what-input/dist/what-input.min.js', array(), true, true);
         wp_enqueue_script('foundation', get_stylesheet_directory_uri() . '/foundation-portfolio/node_modules/foundation-sites/dist/js/foundation.js', array(), true, true);
 //        wp_enqueue_script('javascript', get_stylesheet_directory_uri().'/assets/js/app.js');
         wp_enqueue_script('app', get_stylesheet_directory_uri() . '/assets/js/app.js');
