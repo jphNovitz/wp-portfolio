e99d8ff5ef7b157a6378afeeb6e09193c6310b32
NOVITZ Jean-Philippe <novitz@gmail.com>
2020-11-21 15:36:04 +0100

    single portfolio without images

--- wp-content/themes/jphn_tullipa/template-parts/portfolio/single.php
+++ wp-content/themes/jphn_tullipa/template-parts/portfolio/single.php
@@ -17,7 +17,7 @@ $customFields = get_post_custom();
 //var_dump($langageTerms);
 //echo '</pre>';
 ?>
-<div id ="archive" class="container" style="min-height: 100vh;">
+<div id ="single" class="container" style="min-height: 100vh;">
 <div class="row">
 <h1 class="columns small-12">Portfolio</h1>
 </div>
@@ -67,72 +67,41 @@ while ($site->have_posts()) : $site->the_post();
     </div>
 
     <div class="row m2 ">
-        <div class="columns small-6">
-        <a href="https://facebook.com/jphNovitz" class="webicon github">jphNovitz sur Facebook</a>
-        Site: <a href="<?php echo $customFields['portfolio_url'][0] ?>">
+        <aside class="columns small-6  ">
+            <div class="row aside-links">
+    <span class="columns small-12 name">
+        Site
+    </span> 
+    <span class="columns small-12 description">
+<a href="<?php echo $customFields['portfolio_url'][0] ?>">
                     <?php echo $customFields['portfolio_url'][0] ?>
                 </a>
-        </div>
-        <div class="columns small-6">
-        <p> 
-            <a href="<?php echo $customFields['portfolio_url_git'][0] ?>" class="webicon github">jphNovitz sur Facebook</a> 
+    </span>
+    <span class="columns small-12 text- link-icon">
+
+<i class="fi-link"></i>
+
+</span>
+            </div>
+        </aside>
+        <aside class="columns small-6">
+        <div class="row aside-links">
+            <span class="columns small-12 name">    
             github
-        </p>
+                </span>
+                <span class="columns small-12 description">
         <a href="<?php echo $customFields['portfolio_url_git'][0] ?>">
                     <?php echo $customFields['portfolio_url_git'][0] ?>
                 </a>
-        </div>
+                </span>
+                <span class="columns small-12 link-icon">
+                <i class="fi-social-github"></i>
+                </span>
+        </aside>
     </div>
 
 
 
-    <div class="small-12 columns row  align-self-middle single-title">
-        <h2 class="align-self-middle">
-            <?php the_title(); ?>
-        </h2>
-        <ul class="breadcrumbs text-center">
-            <li>
-                Site: <a href="<?php echo $customFields['portfolio_url'][0] ?>">
-                    <?php echo $customFields['portfolio_url'][0] ?>
-                </a>
-            </li>
-            <li>github: <a href="<?php echo $customFields['portfolio_url_git'][0] ?>">
-                    <?php echo $customFields['portfolio_url_git'][0] ?>
-                </a>
-            </li>
-        </ul>
-    </div>
-
-    <div class="row">
-        <div class="columns small-12 large-8 large-offset-2">
-            <p class="lead">
-                <?php the_excerpt(); ?>
-            </p>
-        </div>
-    </div>
-
-    <div class="row m2">
-        <div class="small-5 small-offset-1 large-2 large-offset-3 portfolio-terms">
-            <ul class="menu simple">
-                <?php foreach ($langageTerms as $language): ?>
-                    <li><?php echo '#'.$language->name ?> </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-        <div class="small-4 small-offset-1 large-2 large-offset-2 portfolio-terms">
-            <ul class="menu simple">
-                <?php foreach ($technoTerms as $techno): ?>
-                    <li><?php echo '#'.$techno->name ?> </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-    </div>
-
-    <div class="row m2 ">
-        <div class="columns large-9">
-            <?php the_content(); ?>
-        </div>
-    </div>
 
 <?php
 endwhile;
