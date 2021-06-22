6069a0eedc4125c2179c66717cb39c0b9e638c0f
Novitz Jean-Philippe <novitz@gmail.com>
2021-06-22 21:20:47 +0200

    new: single.php

--- wp-content/themes/jphn_tullipa/single.php
+++ wp-content/themes/jphn_tullipa/single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php
-get_template_part('template-parts/portfolio/single');
+get_template_part('template-parts/single');
 ?>
 <?php // get_sidebar(); ?>
 <?php get_footer(); ?>
