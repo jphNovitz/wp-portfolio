214c42657cce6e2c77658b30c3685bb47c6a1c59
Novitz Jean-Philippe <novitz@gmail.com>
2021-06-22 21:24:11 +0200

    new: single.php

--- wp-content/themes/jphn_tullipa/template-parts/single.php
+++ wp-content/themes/jphn_tullipa/template-parts/single.php
@@ -7,4 +7,31 @@
     <p> <em><?php echo get_the_excerpt(); ?></em></p>
 
     <p> <?php echo get_the_content(); ?></p>
-</div> 
+</div>
+
+<?php
+
+while ($site->have_posts()) : $site->the_post();
+?>
+
+<div class="row m2 ">
+    <div class="columns small-12 medium-12 align-middle">
+        <h3 class="article-title">
+            <?php echo get_the_excerpt(); ?>
+        </h3>
+        <h2 class=" big lead">
+            <?php the_title(); ?>
+        </h2>
+    </div>
+</div>
+
+<div class="row m2 ">
+    <div class="columns small-12">
+        <?php
+        add_filter('the_content', 'remove_shortcode_from');
+        the_content();
+        remove_filter('the_content', 'remove_shortcode_from');
+        ?>
+    </div>
+</div>
+<?php endwhile; ?>
