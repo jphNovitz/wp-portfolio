3b850b9283fca791528392e3b8e1bfba72e8ee4d
Novitz Jean-Philippe <novitz@gmail.com>
2021-06-22 21:43:56 +0200

    new: single.php

--- wp-content/themes/jphn_tullipa/single.php
+++ wp-content/themes/jphn_tullipa/single.php
@@ -1,19 +1,22 @@
 <?php get_header(); ?>
-    <div class="main single">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <div class="post">
-                    <h1 class="post-title"><?php the_title(); ?></h1>
-                    <p class="post-info">
-                        Posté le <?php the_date(); ?> dans <?php the_category(', '); ?> par <?php the_author(); ?>.
-                    </p>
-                    <div class="post-content">
-                        <?php the_content(); ?>
-                    </div>
 
-                </div>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
-<?php// get_sidebar(); ?>
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <div id="single" class="container" style="min-height: 100vh;">
+            <div class="row">
+                <h1 class="columns small-12"><?php the_title(); ?></h1>
+            </div>
+        
+            <p class="post-info">
+                Posté le <?php the_date(); ?> dans <?php the_category(', '); ?> par <?php the_author(); ?>.
+            </p>
+            <div class="post-content">
+                <?php the_content(); ?>
+            </div>
+
+        </div>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<?php // get_sidebar(); ?>
 <?php get_footer(); ?>
